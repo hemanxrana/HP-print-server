c993dcb25116e8aea6bdb12ea5b2adcc18ee5c16
hemanxrana <119653581+hemanxrana@users.noreply.github.com>
2026-08-15 09:03:06 +0530

    refactor: bind IPP server to persistent job queue

--- mobile_ipp_server.h
+++ mobile_ipp_server.h
@@ -1,15 +1,16 @@
 #pragma once
 #include <Arduino.h>
 #include <WiFi.h>
+#include "mobile_print_queue.h"
 
 class MobileIppServer {
 public:
   using JobHandler = bool (*)(const uint8_t *document, size_t length,
                                const String &documentFormat, uint32_t &jobId,
                                String &error);
-
   explicit MobileIppServer(uint16_t port = 631);
-  void begin(const String &printerName, const String &printerUri, JobHandler handler);
+  void begin(const String &printerName, const String &printerUri,
+             JobHandler handler, MobilePrintQueue *queue);
   void poll();
   bool running() const { return running_; }
 
@@ -19,14 +20,12 @@ private:
   bool running_ = false;
   String printerName_;
   String printerUri_;
+  String printerPath_;
   JobHandler handler_ = nullptr;
-  uint32_t lastJobId_ = 0;
-  String lastJobFormat_;
-  uint8_t lastJobState_ = 3; // pending
-  String lastJobReason_ = "job-incoming";
+  MobilePrintQueue *queue_ = nullptr;
 
   void handleClient(WiFiClient &client);
   bool readHttpBody(WiFiClient &client, uint8_t **body, size_t &length);
-  bool buildResponse(const uint8_t *request, size_t length, uint8_t *response,
-                     size_t capacity, size_t &responseLength);
+  bool buildResponse(const uint8_t *request, size_t length,
+                     uint8_t *response, size_t capacity, size_t &responseLength);
 };

05f980e25ef1b8e829dec1bd250d200e962d3e41
hemanxrana <119653581+hemanxrana@users.noreply.github.com>
2026-08-28 16:43:19 +0530

    Make Android print probe self-contained: add USB host header

---
+++ experiments/android_print_probe/usb_host_manager.h
@@ -0,0 +1,47 @@
+#pragma once
+#include <Arduino.h>
+#include "usb_device.h"
+
+class UsbHostManager {
+public:
+  enum State : uint8_t {
+    STOPPED,
+    RUNNING,
+    ENUMERATING,
+    DEVICE_ATTACHED,
+    PRINTER_READY,
+    ERROR
+  };
+
+  bool begin();
+  void poll();
+  State state() const { return state_; }
+  const UsbDeviceInfo &device() const { return device_; }
+  const String &lastError() const { return error_; }
+
+  const UsbPrinterInterfaceInfo *selectedInterface() const;
+  const UsbPrinterInterfaceInfo *statusInterface() const;
+
+  bool bulkWrite(const uint8_t *data, size_t length, size_t &accepted,
+                 uint32_t timeoutMs, String &error);
+
+  bool portStatusValid() const { return device_.portStatus.valid; }
+  uint8_t portStatusValue() const { return device_.portStatus.value; }
+  bool portStatusError() const { return device_.portStatus.error; }
+  bool portStatusSelected() const { return device_.portStatus.selected; }
+  bool portStatusPaperEmpty() const { return device_.portStatus.paperEmpty; }
+  const UsbPortStatus &portStatus() const { return device_.portStatus; }
+  bool hasSeparateStatusInterface() const { return device_.statusInterfaceSeparate; }
+
+  void onPortStatusTransfer(bool valid, uint8_t value, const String &error);
+  void onEnumerationStarted();
+  void onEnumerated(const UsbDeviceInfo &info);
+  void onDetached();
+  void onEnumerationError(const String &error);
+
+private:
+  State state_ = STOPPED;
+  UsbDeviceInfo device_;
+  String error_;
+  bool started_ = false;
+};

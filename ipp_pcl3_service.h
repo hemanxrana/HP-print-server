c0357dc1c57572bd3a91c483faa7e6f50bb6cfd5
hemanxrana <119653581+hemanxrana@users.noreply.github.com>
2026-08-28 22:12:14 +0530

    Track IPP job completion state

--- ipp_pcl3_service.h
+++ ipp_pcl3_service.h
@@ -14,10 +14,15 @@ public:
 
 private:
   void handleClient(WiFiClient client);
+  void refreshJobState();
 
   UsbPrinterBackend &printer_;
   bool started_ = false;
   bool clientActive_ = false;
   uint64_t lastJobBytes_ = 0;
   String lastError_;
+  uint32_t nextJobId_ = 1;
+  uint32_t lastJobId_ = 0;
+  uint8_t lastJobState_ = 0; // IPP job-state enum: 5 processing, 8 aborted, 9 completed
+  String lastJobReason_;
 };
